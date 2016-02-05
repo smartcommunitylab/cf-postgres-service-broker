1717e357e5e2aabd959008468be06948a1081f64
Роман <kazhamiakin@gmail.com>
2016-02-05 14:33:35 +0100

    credentials made transitional

--- src/main/java/org/springframework/cloud/servicebroker/postgresql/model/ServiceInstanceBinding.java
+++ src/main/java/org/springframework/cloud/servicebroker/postgresql/model/ServiceInstanceBinding.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Lob;
+import javax.persistence.Transient;
 
 /**
  * A binding to a service instance
@@ -18,10 +18,12 @@ public class ServiceInstanceBinding {
 	@Id
 	private String id;
 	private String serviceInstanceId;
-	@Lob
-	private Map<String,Object> credentials = new HashMap<>();
 	private String syslogDrainUrl;
 	private String appGuid;
+	private String uri;
+	
+	@Transient
+	private Map<String,Object> credentials = new HashMap<>();
 
 	public ServiceInstanceBinding() {
 	}
@@ -46,6 +48,10 @@ public class ServiceInstanceBinding {
 	}
 
 	public Map<String, Object> getCredentials() {
+		if (credentials == null || credentials.isEmpty())  {
+			credentials = new HashMap<>();
+			credentials.put("uri", getUri());
+		}
 		return credentials;
 	}
 
@@ -65,4 +71,19 @@ public class ServiceInstanceBinding {
 		return appGuid;
 	}
 
+	/**
+	 * @return the uri
+	 */
+	public String getUri() {
+		return uri;
+	}
+
+	/**
+	 * @param uri the uri to set
+	 */
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	
 }
